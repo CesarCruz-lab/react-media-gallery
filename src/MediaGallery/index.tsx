27ef45bcd33215ba98eb6999092e04cbe57e96b7
CesarCruz-lab <rbcesar86@gmail.com>
2022-02-06 17:39:28 -0500

    inline preview

--- src/MediaGallery/index.tsx
+++ src/MediaGallery/index.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
+import InlinePreview from 'components/Preview/Inline/InlinePreview';
+
+const MediaGalleryContainer = styled('div')`
+  width: 95vw;
+  max-width: ${props => props.theme.breakpoints?.tablet}px;
+  height: max-content;
+  margin: 0 auto;
+  padding: ${({ theme }) => theme.spacing()};
+  border-radius: ${({ theme }) => theme.spacing(0.5)};
+  border-color: ${({ theme }) => theme.grey[100]};
+  border-style: solid;
+  border-width: 1px;
+`;
 
 const MediaGallery: React.FC = () => {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <MediaGalleryContainer>
+      <InlinePreview />
+    </MediaGalleryContainer>
   );
 };
 
---
+++ src/components/Preview/Inline/InlinePreview.tsx
@@ -0,0 +1,57 @@
+import React, { useCallback, useRef } from 'react';
+import { useMediaGallery } from 'hooks/useMediaGallery';
+import {
+  InlinePreviewBox,
+  InlinePreviewItem,
+  InlinePreviewList,
+  PreviewButton,
+  PreviewContainer,
+} from './InlinePreview.style';
+
+// Icons
+import { BsArrowLeftShort } from 'react-icons/bs';
+import { BsArrowRightShort } from 'react-icons/bs';
+import Video from './VideoPreview';
+
+const InlinePreview: React.FC = () => {
+  const { gallery } = useMediaGallery();
+  const inlinePreviewListRef = useRef<HTMLUListElement>(null);
+  const inlinePreviewBoxRef = useRef<HTMLDivElement>(null);
+
+  const next = useCallback(() => {
+    const listHeight = Number(inlinePreviewListRef.current?.getBoundingClientRect().height);
+    const diff = Number(inlinePreviewBoxRef.current?.scrollLeft);
+
+    inlinePreviewBoxRef.current?.scrollTo(diff + listHeight, 0);
+  }, [inlinePreviewBoxRef, inlinePreviewListRef]);
+
+  const previous = useCallback(() => {
+    const listHeight = Number(inlinePreviewListRef.current?.getBoundingClientRect().height);
+    const diff = Number(inlinePreviewBoxRef.current?.scrollLeft);
+
+    inlinePreviewBoxRef.current?.scrollTo(diff - listHeight, 0);
+  }, [inlinePreviewBoxRef, inlinePreviewListRef]);
+
+  return (
+    <PreviewContainer>
+      <PreviewButton onClick={previous}>
+        <BsArrowLeftShort style={{ width: 24, height: 24 }} />
+      </PreviewButton>
+      <InlinePreviewBox ref={inlinePreviewBoxRef}>
+        <InlinePreviewList ref={inlinePreviewListRef}>
+          {gallery?.map(media => (
+            <InlinePreviewItem key={media.id}>
+              {/image/.test(media.mimetype) && <img src={media.path} alt={media.mimetype} />}
+              {/video/.test(media.mimetype) && <Video media={media} />}
+            </InlinePreviewItem>
+          ))}
+        </InlinePreviewList>
+      </InlinePreviewBox>
+      <PreviewButton onClick={next}>
+        <BsArrowRightShort style={{ width: 24, height: 24 }} />
+      </PreviewButton>
+    </PreviewContainer>
+  );
+};
+
+export default InlinePreview;
